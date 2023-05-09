40dd6cba93a0c126853d3a1fac12bb1372e4930e
PedroAugusto <pedro.pazinatto@grupointegrado.br>
2023-05-09 20:12:48 -0300

    Teste

---
+++ file-system.js
@@ -0,0 +1,21 @@
+// const fs = require ("fs");
+// const fsPromise = require ("fs/promises")
+
+// fs.readdir("./", (err, files) => {
+//     if (err) {
+//     console.log(err);
+//     return;
+//     }
+//     console.log(files);
+//     }
+//     );
+
+
+//     fsPromise.readdir(".").then((files) => {
+//         console.log(files);
+//     }).catch((err) => {
+//         console.log(err);
+//     } )
+
+
+-- 
